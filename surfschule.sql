3023b38e96665e9c2228213c7bd86847fedb5c4c
Kotaro Osaka <126588189+kotaro-osaka@users.noreply.github.com>
2025-09-22 14:17:59 +0200

    latest

--- surfschule.sql
+++ surfschule.sql
@@ -1,16 +1,16 @@
-CREATE DATABASE IF NOT EXISTSsurfschule;
-USE surfschule;
+CREATE DATABASE IF NOT EXISTS `surfschule`;
+USE `surfschule`;
 
-CREATE TABLE IF NOT EXISTS unterkunft(
+CREATE TABLE IF NOT EXISTS `unterkunft`(
     unterkunft_id INT AUTO_INCREMENT PRIMARY KEY,
     bezeichnung VARCHAR(50),
     groesse INT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_german2_ci;
+) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_german2_ci;
 
-CREATE TABLE IF NOT EXISTS schueler(
+CREATE TABLE IF NOT EXISTS `schueler`(
     schueler_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50),
     alter INT,
     unterkunft_id INT,
     FOREIGN KEY (unterkunft_id) REFERENCES unterkunft(unterkunft_id) ON UPDATE NO ACTION ON DELETE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_german2_ci;
+) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_german2_ci;
